27b3ff23603b8be2d148d1acbd4384db8044235a
rodrigobbrozinga <rodrigobbrozinga@gmail.com>
2021-07-05 10:59:48 -0300

    Comecando a implementar o tabuleiro

--- ProjetoXadrezConsole/Program.cs
+++ ProjetoXadrezConsole/Program.cs
@@ -7,11 +7,7 @@ namespace ProjetoXadrezConsole
     {
         static void Main(string[] args)
         {
-            Posicao p;
-
-            p = new Posicao(3, 4);
-
-            Console.WriteLine("Posição: " + p);
+            Tabuleiro tab = new Tabuleiro(8, 8);
         }
     }
 }
